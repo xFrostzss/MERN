ee43af7a144456ef31b297ab89f30f1c63d31e8d
Kayky <kaykysilva032006@gmail.com>
2025-12-05 23:11:40 -0300

    Segundo commit (" So pra salvar ")

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,16 @@ import "dotenv/config.js";
 
 const app = express();
 
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(process.env, MONGO_URI);
+    console.log("Connected to the database successfully.");
+  }catch(error) {
+    console.error("Database connection failed:", error);
+  }
+}
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000. CTRL + C to stop.');
+  process.exit(1);
 });
